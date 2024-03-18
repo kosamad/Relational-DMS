1784b731e1652e9f442db6990c9d4f3728cdd6fa
kosamad samad <KarenLSamad@gmail.com>
2024-03-18 13:01:29 +0000

    python for database queries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,12 +1,39 @@
 import psycopg2
 
 # connect to chinook database using connect() method
-connection = psycopg2.connect(database = "chinnok")
+connection = psycopg2.connect(database = "chinook")
 
 # build a cursor object of the database (from what we query)
 cursor = connection.cursor()
 
-# fetch the results from the cursor (multiple)
+# coded out prev queries to avoid duplicates
+
+# Query 1 - select all records from the "artist" table
+# the quote use here has to be EXACTLY THE SAME
+# cursor.execute('SELECT * FROM "artist"')
+
+# Query 2 - select only the "name" column from the "artist" table
+# cursor.execute('SELECT "name" FROM "artist"')
+
+# Query 3 - select only Queen from the artist table (therefore use with fetchone)
+# cursor.execute('SELECT * FROM "artist" WHERE "name" = %s', ["Queen"])
+
+# Query 4 - select only artist_id #51 from the artist table
+# cursor.execute('SELECT * FROM "artist" WHERE "artist_id" = %s', [51])
+
+# # Query 5 - select on albums with the artist ID #51 on the album table
+# cursor.execute('SELECT * FROM "album" WHERE "artist_id" = %s', [51])
+
+# Query 5 - select on albums with the artist ID #51 on the album table
+# cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
+
+# # Query 6 - select only 'Jack Johnson' from the artist table (therefore use with fetchone)
+# cursor.execute('SELECT * FROM "artist" WHERE "name" = %s', ["Jack Johnson"])
+
+# Query 6 - select only 'test' from the artist table- doesn't work!
+cursor.execute('SELECT * FROM "artist" WHERE "name" = %s', ["test"])
+
+# # fetch the results from the cursor (multiple)
 results = cursor.fetchall()
 
 # fetch the result (single)
